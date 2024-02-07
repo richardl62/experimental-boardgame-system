ef23d4b860e0cf8de39f5b849a2bcaaec3ce3a9d
Richard L <richard.latham@yahoo.co.uk>
2024-02-07 18:24:35 +0000

    Ensure games and board have the same keys

--- client/src/boards/index.ts
+++ client/src/boards/index.ts
@@ -1,7 +1,8 @@
+import { games } from '../shared/games';
 import tictactoe from './tictactoe/board';
 import plusminus from './plusminus/board';
 
-export const boards = {
+export const boards : Record<keyof typeof games, () => JSX.Element> = {
   tictactoe,
-  plusminus
-};
+  plusminus,
+} as const;
