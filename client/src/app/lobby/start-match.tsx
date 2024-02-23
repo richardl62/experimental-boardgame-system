4bc7f39a0cd841b2a8332af22569900256c58147
Richard L <richard.latham@yahoo.co.uk>
2024-02-23 16:34:27 +0000

    Move lobby into app

--- client/src/app/lobby/start-match.tsx
+++ client/src/app/lobby/start-match.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { useAsyncCallback } from "react-async-hook";
 import { AppGame } from "../../app-game-support";
-import { loadingOrError, LoadingOrError } from "../utils/async-status";
-import { BoxWithLegend } from "../utils/box-with-legend";
+import { loadingOrError, LoadingOrError } from "../../utils/async-status";
+import { BoxWithLegend } from "../../utils/box-with-legend";
 import { createMatch } from "./lobby-tools";
 import { openOnlineMatchPage } from "../url-params";
 import { OfflineOptions } from "../offline-options";
 import { OptionValues, SpecifiedValues } from "../option-specification/types";
 import { InputValues } from "../option-specification/input-values";
 import { defaultNumPlayers } from "../../app-game-support/app-game";
-import { sAssert } from "../utils/assert";
+import { sAssert } from "../../utils/assert";
 
 
 export function StartMatch(props: {
