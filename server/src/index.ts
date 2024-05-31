237dbda8a5caa73f9c29bd1439e7a1a8e3b5aeeb
Richard L <richard.latham@yahoo.co.uk>
2024-05-31 15:55:47 +0100

    prevent cors errors from localhost:3000

--- server/src/index.ts
+++ server/src/index.ts
@@ -36,6 +36,12 @@ function sendError(operation: string, res: Response, err: unknown) {
   res.status(400).send(`${operation}: ${message}`);
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // or "*" to cover all domains
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Run all the functions provided by the LobbyClient
 // The name of the function to run (createMatch, joinMatch etc)
 // is provided as a query parameter.
