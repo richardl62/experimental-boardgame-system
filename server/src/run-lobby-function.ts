93a1d275601f3d97926951f25e87f3a9b12c3e8e
Richard L <richard.latham@yahoo.co.uk>
2024-06-19 11:14:44 +0100

    Smallish changes to server

--- server/src/run-lobby-function.ts
+++ server/src/run-lobby-function.ts
@@ -1,5 +1,22 @@
 import { ParsedQs } from 'qs';
 
+function requireString(param: unknown) : string {
+  if(typeof param === "string") {
+    return param;
+  }
+  throw new Error("Bad type when expecting string");
+}
+
+function requireInteger(param: unknown) : number {
+  const str = requireString(param);
+  const num = parseInt(str);
+  if(isNaN(num)) {
+    throw new Error(`"${str}" is not an integer`);
+  }
+
+  return num;
+}
+
 export function runLobbyFunction(query: ParsedQs) {
   console.log("Lobby function called:", query, query.func);
   throw new Error('Function not implemented.');
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,40 +1,14 @@
 import express, { Express, Request, Response , Application } from 'express';
 import { Server, WebSocket as WSWebSocket  } from 'ws'; // Import the ws library
 
-import { games } from './shared/games';
-import { GameDefinition } from './shared/game-definition';
-import { WsMoveData } from './shared/types';
-import { Match } from './match';
 import url from 'url';
 import { Matches } from './matches';
 import { runLobbyFunction } from './run-lobby-function';
 
-const app: Application = express();
 const port = process.env.PORT || 8000;
-
 const matches = new Matches();
 
-function requireString(param: unknown) : string {
-  if(typeof param === "string") {
-    return param;
-  }
-  throw new Error("Bad type when expecting string");
-}
-
-function requireInteger(param: unknown) : number {
-  const str = requireString(param);
-  const num = parseInt(str);
-  if(isNaN(num)) {
-    throw new Error(`"${str}" is not an integer`);
-  }
-
-  return num;
-}
-
-function sendError(operation: string, res: Response, err: unknown) {
-  const message = err instanceof Error ? err.message : "unknown error";
-  res.status(400).send(`${operation}: ${message}`);
-}
+const app: Application = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -50,7 +24,8 @@ app.get('/lobby', (req: Request, res: Response) => {
     const result = runLobbyFunction(req.query);
     res.send(JSON.stringify(result));
   } catch (err) {
-      sendError("startmatch", res, err);
+    const message = err instanceof Error ? err.message : "unknown error";
+    res.status(400).send(`Lobby error: ${message}`);
   }
 });
 
