7d4ffd4181aa62ec63d926ca46873d5636116299
Richard L <richard.latham@yahoo.co.uk>
2024-09-26 17:32:08 +0100

    Add some comments

--- server/src/shared/server-move-response.ts
+++ server/src/shared/server-move-response.ts
@@ -3,8 +3,10 @@ import { MatchData } from "./match-data";
 // The data returned from the server after move.
 // Also returned after a connection.
 export type ServerMoveResponse = {
+    /** Error typically related to the previous move, but could show other bugs.
+     */
     error?: string;
 
-    
+    /** Match data will be missing following certain errors */
     matchData?: MatchData; 
 };
